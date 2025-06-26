3a22a02dbcc9e91011ae18ca7f99ae683b38935c
Likhaya <kalimashelikhaya3@gmail.com>
2025-06-26 19:45:32 +0200

    freecodecamp lessons 1 - 20

---
+++ day-01-variables/index.js
@@ -0,0 +1,44 @@
+// learn how to create/initialize a variable in js
+// and give it a value
+
+let variable = "hello";
+
+// a variable can be reassigned without using let
+
+variable = "worlds";
+
+// this is an uninitialized variable in js
+
+let variableTwo;
+
+// you can also reassign a variable like so
+
+variableTwo = variable;
+
+// this is how you can log your variables
+
+console.log(variable);
+console.log(variableTwo);
+
+// an unitialized variable logs the data type undefined
+let variableThree;
+
+console.log(variableThree);
+
+// we learn about assigning number values
+
+let number = 1;
+
+// we can also log calculations like
+
+console.log(number + 10);
+
+// this is an array, arrays hold values or elements inside square brackets.
+// the values/elements are seperated by commas
+
+let array = ["This", "is", "an", "array"];
+
+// to log an array you need to access the index of the array
+// the index is basically the position of the value starting from 0
+
+console.log(array[0]);
